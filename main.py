0818849367c60eae4174f158d5c29750074acfdd
MGlaJ <165914@student.uwm.edu.pl>
2022-03-08 22:25:52 +0100

    Initial commit lab1

---
+++ main.py
@@ -0,0 +1,131 @@
+# zadanie_1
+# a = 'Ania'
+# b = 'Basia'
+# c = 4
+# d = 6
+# e = 2.0
+# f = 3.0
+# g = 1+1j
+# h = 2+3j
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+# print(h)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+#
+# zadanie_2
+# x = 2.0
+# y = 8.0
+# z = 3.0
+# print(x+y) #dodawanie
+# print(y-x) #odejmowanie
+# print(x*y) #mnożenie
+# print(y/x) #dzielenie
+# print(y/z) #dzielenie
+# print(y//z) #dzielenie_bez_reszty
+# print(y%z) #reszta_z_dzielenia
+# print(z/x) #dzielenie
+# print(z%x) #reszta_z_dzielenia
+# print(z**x) #potęgowanie_metoda_1
+# potega = pow(y,x) #potęgowanie_metoda_2
+# print(potega)
+#
+# #zadanie_3
+# a1 = 2
+# a2 = 3
+# a3 = 4
+# #operator_przyrostkowy_1
+# a1 += 1
+# print(a1)
+# a2 += 2
+# print(a2)
+# a3 += 3
+# print(a3)
+# #operator_przyrostkowy_2
+# a1 -= 2
+# a2 -= 1.5
+# a3 -= 2
+# print(a1, a2, a3)
+# #operator_przyrostkowy_3
+# a1 /= 3.0
+# a2 /= 0.5
+# a3 /= 5
+# print(a1, a2, a3)
+# #operator_przyrostkowy_4
+# a1 *= 6
+# a2 *= 2
+# a3 *= 10
+# print (a1, a2, a3)
+# #operator_przyrostkowy_5
+# a1 **= 3
+# a2 **= 2
+# a3 **= 4
+# print (a1, a2, a3)
+# #operator_przyrostkowy_6
+# a1 %= 5
+# a2 %= 40
+# a3 %= 700
+# print (a1, a2, a3)
+#
+# zadanie_4
+# import math
+# b1 = math.exp(10)
+# print(b1)
+# b2 = (math.log(5+(math.sin(8)**2)))**(1/6)
+# print(b2)
+# b3 = (math.floor(3.55))
+# print(b3)
+# b4 = (math.ceil(4.80))
+# print(b4)
+#
+# zadanie_5
+# imie = 'MARTA'
+# nazwisko = 'GLAZ'
+# print (imie.capitalize() + ' ' + nazwisko.capitalize())
+#
+# zadanie_6
+# piosenka = "I'm falling\n" \
+#            "In all the good times,\n" \
+#            "I find myself longing for change\n" \
+#            "And in the bad times,\n" \
+#            "I fear myself\n" \
+#            "I'm off the deep end,\n" \
+#            "watch as I dive in\n" \
+#            "I'll never meet the ground\n" \
+#            "Crash through the surface,\n" \
+#            "where they can't hurt us\n" \
+#            "We're far from the shallow now\n" \
+#            "In the sha-ha, sha-ha-llow\n" \
+#            "In the sha-ha-sha-la-la-la-llow\n" \
+#            "In the sha-ha, sha-ha-llow\n" \
+#            "We're far from the shallow now."
+# print(piosenka)
+# print(piosenka.count("sha"))
+#
+# zadanie_7
+# stolica = 'Waga Dugu'
+# print(stolica[1])
+# print(len(stolica))
+# print(stolica[8])
+#
+# zadanie_8
+# piosenka = "In the sha-ha, sha-ha-llow In the sha-ha-sha-la-la-la-llow In the sha-ha, sha-ha-llow We're far from the shallow now."
+# print(piosenka.split(" "))
+#
+# zadanie_9
+# wyraz = 'Warszawa'
+# liczba = 2.555
+# szesnastkowa = (0x7b)
+# print('{0:s}, {1:.3f}, {2:X}'.format(wyraz, liczba, szesnastkowa))
+#
